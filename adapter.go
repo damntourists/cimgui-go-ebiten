84fd624505ac40baa65485e26042141c1eb279ea
Brett Bronson <brett.bronson@gmail.com>
2024-03-14 21:04:54 -0700

    ...

--- adapter.go
+++ adapter.go
@@ -5,11 +5,71 @@ import (
 	imgui "github.com/damntourists/cimgui-go-lite"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"image"
 	"runtime"
 )
 
 var currentAdapter *EbitenAdapter
+
+var keys = map[imgui.Key]int{
+	imgui.KeyTab:        int(ebiten.KeyTab),
+	imgui.KeyLeftArrow:  int(ebiten.KeyLeft),
+	imgui.KeyRightArrow: int(ebiten.KeyRight),
+	imgui.KeyUpArrow:    int(ebiten.KeyUp),
+	imgui.KeyDownArrow:  int(ebiten.KeyDown),
+	imgui.KeyPageUp:     int(ebiten.KeyPageUp),
+	imgui.KeyPageDown:   int(ebiten.KeyPageDown),
+	imgui.KeyHome:       int(ebiten.KeyHome),
+	imgui.KeyEnd:        int(ebiten.KeyEnd),
+	imgui.KeyInsert:     int(ebiten.KeyInsert),
+	imgui.KeyDelete:     int(ebiten.KeyDelete),
+	imgui.KeyBackspace:  int(ebiten.KeyBackspace),
+	imgui.KeySpace:      int(ebiten.KeySpace),
+	imgui.KeyEnter:      int(ebiten.KeyEnter),
+	imgui.KeyEscape:     int(ebiten.KeyEscape),
+	imgui.KeyA:          int(ebiten.KeyA),
+	imgui.KeyC:          int(ebiten.KeyC),
+	imgui.KeyV:          int(ebiten.KeyV),
+	imgui.KeyX:          int(ebiten.KeyX),
+	imgui.KeyY:          int(ebiten.KeyY),
+	imgui.KeyZ:          int(ebiten.KeyZ),
+}
+
+func sendInput(io *imgui.IO, inputChars []rune) []rune {
+	// Ebiten hides the LeftAlt RightAlt implementation (inside the uiDriver()), so
+	// here only the left alt is sent
+	if ebiten.IsKeyPressed(ebiten.KeyAlt) {
+		io.SetKeyAlt(true)
+	} else {
+		io.SetKeyAlt(false)
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyShift) {
+		io.SetKeyShift(true)
+	} else {
+		io.SetKeyShift(false)
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyControl) {
+		io.SetKeyCtrl(true)
+	} else {
+		io.SetKeyCtrl(false)
+	}
+	// TODO: get KeySuper somehow (GLFW: KeyLeftSuper    = Key(343), R: 347)
+	inputChars = ebiten.AppendInputChars(inputChars)
+	if len(inputChars) > 0 {
+		io.AddInputCharactersUTF8(string(inputChars))
+		inputChars = inputChars[:0]
+	}
+	for ik, iv := range keys {
+		if inpututil.IsKeyJustPressed(ebiten.Key(iv)) {
+			io.AddKeyEvent(ik, true)
+		}
+		if inpututil.IsKeyJustReleased(ebiten.Key(iv)) {
+			io.AddKeyEvent(ik, false)
+		}
+	}
+	return inputChars
+}
 
 // Adapter should proxy calls to backend.
 type Adapter interface {
@@ -49,9 +109,10 @@ type EbitenAdapter struct {
 	game    ebiten.Game
 	loop    func()
 
-	ClipMask bool
-	lmask    *ebiten.Image
-	cliptxt  string
+	ClipMask   bool
+	lmask      *ebiten.Image
+	cliptxt    string
+	inputChars []rune
 }
 
 type GameProxy struct {
@@ -215,16 +276,6 @@ func NewEbitenAdapter() *EbitenAdapter {
 	b.ctx = imgui.CreateContext()
 	imgui.ImNodesCreateContext()
 
-	//fonts := imgui.CurrentIO().Fonts()
-	//_, _, _, _ = fonts.GetTextureDataAsRGBA32()
-
-	b.SetBeforeRenderHook(func() {
-		// TODO
-	})
-	b.SetAfterRenderHook(func() {
-		// TODO
-	})
-
 	bb := (imgui.Backend[EbitenWindowFlags])(b)
 	bb.SetKeyCallback(func(key, scanCode, action, mods int) {
 		println("key", key, "scanCode", scanCode, "action", action, "mods", mods)
@@ -232,9 +283,11 @@ func NewEbitenAdapter() *EbitenAdapter {
 	createdBackend, _ := imgui.CreateBackend(bb)
 
 	a := EbitenAdapter{
-		backend:  createdBackend,
-		ClipMask: true,
+		backend:    createdBackend,
+		ClipMask:   true,
+		inputChars: make([]rune, 0, 256),
 	}
+	a.setKeyMapping()
 
 	runtime.SetFinalizer(&a, (*EbitenAdapter).finalize)
 
@@ -248,12 +301,10 @@ func (a *EbitenAdapter) finalize() {
 }
 
 func (a *EbitenAdapter) SetGame(g ebiten.Game) {
-
 	a.game = GameProxy{
 		game:   g,
 		filter: ebiten.FilterNearest,
 	}
-
 }
 
 func (a *EbitenAdapter) Game() ebiten.Game {
@@ -289,4 +340,14 @@ func (a *EbitenAdapter) Run(f func()) {
 
 func (a *EbitenAdapter) SetAfterCreateContextHook(hook func()) {
 	a.backend.SetAfterCreateContextHook(hook)
+}
+
+func (a *EbitenAdapter) setKeyMapping() {
+	// Keyboard mapping. ImGui will use those indices to peek into the io.KeysDown[] array.
+	/*
+		io := cimgui.GetIO()
+		for imguiKey, nativeKey := range keys {
+			// io.KeyMap(int(imguiKey), nativeKey)
+		}
+	*/
 }
